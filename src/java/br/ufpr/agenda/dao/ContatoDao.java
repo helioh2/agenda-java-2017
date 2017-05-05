3557510e7b1c58ab28c57af7be5800fda62d2da3
Helio <heliohenrique@ufpr.br>
2017-05-05 18:50:39 -0300

    Outras operações do DAO.

--- src/java/br/ufpr/agenda/dao/ContatoDao.java
+++ src/java/br/ufpr/agenda/dao/ContatoDao.java
@@ -10,7 +10,11 @@ import br.ufpr.agenda.modelo.Contato;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -19,36 +23,114 @@ import java.util.logging.Logger;
  * @author ufpr
  */
 public class ContatoDao {
-    
-    private Connection con;
+
+    private Connection conexao;
 
     public ContatoDao() {
-        ConnectionFactory factory = new ConnectionFactory();      
-        con = factory.getConnection();
+        ConnectionFactory factory = new ConnectionFactory();
+        conexao = factory.getConnection();
     }
-    
-    
+
     public void adiciona(Contato contato) {
-        
-        String sql = "INSERT INTO contatos" +
-                    " (nome,email,endereco,dataNascimento)" +
-                    " VALUES (?,?,?,?)";
-        
+
+        String sql = "INSERT INTO contatos"
+                + " (nome,email,endereco,dataNascimento)"
+                + " VALUES (?,?,?,?)";
+
         try {
-            PreparedStatement stmt = con.prepareStatement(sql);
-            
+            PreparedStatement stmt = conexao.prepareStatement(sql);
+
             stmt.setString(1, contato.getNome());
             stmt.setString(2, contato.getEmail());
             stmt.setString(3, contato.getEndereco());
             Date dataSql = new Date(contato.getDataNascimento().getTimeInMillis());
             stmt.setDate(4, dataSql);
-            
+
             stmt.execute();
-            stmt.close();            
-            
+            stmt.close();
+
         } catch (SQLException ex) {
             System.out.println(ex);
         }
     }
-    
+
+    public List<Contato> getLista() {
+
+        try {
+
+            List<Contato> contatos = new ArrayList<Contato>();
+
+            PreparedStatement stmt = this.conexao.prepareStatement("select * from contatos");
+
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+
+                // criando o objeto Contato
+                Contato contato = new Contato();
+                contato.setId(rs.getLong("id"));
+                contato.setNome(rs.getString("nome"));
+                contato.setEmail(rs.getString("email"));
+                contato.setEndereco(rs.getString("endereco"));
+
+                // montando a data através do Calendar
+                Calendar data = Calendar.getInstance();
+                data.setTime(rs.getDate("dataNascimento"));
+                contato.setDataNascimento(data);
+
+                // adicionando o objeto à lista
+                contatos.add(contato);
+
+            }
+            rs.close();
+            stmt.close();
+            return contatos;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public void altera(Contato contato) {
+
+        String sql = "update contatos set nome=?, email=?, endereco=?,"
+                + "dataNascimento=? where id=?";
+
+        try {
+            PreparedStatement stmt = conexao.prepareStatement(sql);
+
+            stmt.setString(1, contato.getNome());
+            stmt.setString(2, contato.getEmail());
+            stmt.setString(3, contato.getEndereco());
+            stmt.setDate(4, new Date(contato.getDataNascimento().getTimeInMillis()));
+            stmt.setLong(5, contato.getId());
+
+            stmt.execute();
+            stmt.close();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public void remove(Contato contato) {
+
+        try {
+
+            PreparedStatement stmt = conexao.prepareStatement("delete"
+                    + " from contatos where id=?");
+            
+            stmt.setLong(1, contato.getId());
+            
+            stmt.execute();
+            stmt.close();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
 }
--- src/java/br/ufpr/agenda/modelo/ContatoTest.java
+++ src/java/br/ufpr/agenda/modelo/ContatoTest.java
@@ -7,6 +7,7 @@ package br.ufpr.agenda.modelo;
 
 import br.ufpr.agenda.dao.ContatoDao;
 import java.util.Calendar;
+import java.util.List;
 
 /**
  *
@@ -27,6 +28,27 @@ public class ContatoTest {
         bd.adiciona(contato1);
         
         System.out.println(contato1.getNome());
+        
+        //Busca:       
+        List<Contato> contatos = bd.getLista();
+        for (Contato contato: contatos){
+            System.out.println(contato.getId()+" "+contato.getNome());
+        }
+        
+        //Altera:
+        int id = 2;
+        Contato contatoaltera = contatos.get(id);
+        contatoaltera.setNome(contatoaltera.getNome() + " alterado");
+        bd.altera(contatoaltera);
+        
+        //Remove:
+        int id2 = 3;
+        Contato contatoremove = contatos.get(id2);
+        bd.remove(contatoremove);
+        
+        
+        
+        
         
         
     }
